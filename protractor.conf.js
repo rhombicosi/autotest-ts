0f100dbc00fcd84e00001bb8acb81602bb239472
Olia Karelkina <volha.karelkina@itechart-group.com>
2017-09-06 23:50:37 +0300

    -

--- protractor.conf.js
+++ protractor.conf.js
@@ -11,7 +11,7 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome',
     chromeOptions: {
-      args: [ "--headless", "--disable-gpu", "--window-size=1024x780" ]
+      args: [ "--headless", "--disable-gpu", "--window-size=1024x768" ]
     }
   },
   directConnect: true,
